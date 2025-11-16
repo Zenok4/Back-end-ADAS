b9ff58374987c0aa743e4f4b341f0378cef3be35
tuilalinh <lepnhatlinh1@dtu.edu.vn>
2025-11-16 21:26:00 +0700

    sửa lại .data

--- services/ai/sign_service.py
+++ services/ai/sign_service.py
@@ -12,24 +12,19 @@ class SignService:
         print(f"Full AI endpoint URL: {self.ai_server_url}")
 
     def predict_sign(self, image_base64: str):
-        # Payload gửi sang AI server
         payload = {"image_base64": image_base64}
 
         try:
             response = requests.post(self.ai_server_url, json=payload, timeout=30)
 
             if not response.ok:
-                return response_error(
-                    code=response.status_code,
-                    message="Error from AI server",
-                )
+                return {
+                    "error": "Error from AI server",
+                    "code": response.status_code
+                }
 
-            return response_success(
-                data=response.json(),
-                key="data",
-                message="Analyze sign success",
-                code=HttpCode.success
-            )
+            ai_res = response.json()
+            return ai_res.get("data", [])
 
         except requests.exceptions.RequestException as e:
             return {
